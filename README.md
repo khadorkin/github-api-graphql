09347111384df56a3115b82ba26bcae712732fb2
Ricardo Mendes Portugal <ricardofmportugal@gmail.com>
2016-08-23 17:48:37 +0100

    add more user fields; update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![Build Status](https://travis-ci.org/rportugal/graphql-github-js.svg?branch=master)](https://travis-ci.org/rportugal/graphql-github-js)
 
-Full example using GraphQL-JS, DataLoader, Flow
+Full example GraphQL server using [GraphQL JS](https://github.com/graphql/graphql-js), [DataLoader](https://github.com/facebook/dataloader), [Flow](https://github.com/facebook/flow) .
--- src/test/schema/user_query_test.js
+++ src/test/schema/user_query_test.js
@@ -10,7 +10,32 @@ const query = `
     id
     avatar_url
     gravatar_id
-    url  
+    url
+    html_url
+    followers_url
+    following_url
+    gists_url
+    starred_url
+    subscriptions_url
+    organizations_url
+    repos_url
+    events_url
+    received_events_url
+    type
+    site_admin
+    name
+    company
+    blog
+    location
+    email
+    hireable
+    bio
+    public_repos
+    public_gists
+    num_followers
+    num_following
+    created_at
+    updated_at
   }
 }
 `;
@@ -36,13 +61,39 @@ describe('User query', () => {
 
   it('returns the base fields', async() => {
     const result = await runQuery(query);
-    expect(nock.isDone()).to.equal(true, "Didn't hit all mock endpoints!");
+    console.log(result);
 
+    expect(nock.isDone()).to.equal(true, "Didn't hit all mock endpoints!");
     const user = result.data.user;
     expect(user.login).to.equal('rportugal');
     expect(user.id).to.equal(784082);
     expect(user.avatar_url).to.equal('https://avatars.githubusercontent.com/u/784082?v=3');
     expect(user.gravatar_id).to.equal('');
     expect(user.url).to.equal('https://api.github.com/users/rportugal');
+    expect(user.html_url).to.equal('https://github.com/rportugal');
+    expect(user.followers_url).to.equal('https://api.github.com/users/rportugal/followers');
+    expect(user.following_url).to.equal('https://api.github.com/users/rportugal/following{/other_user}');
+    expect(user.gists_url).to.equal('https://api.github.com/users/rportugal/gists{/gist_id}');
+    expect(user.starred_url).to.equal('https://api.github.com/users/rportugal/starred{/owner}{/repo}');
+    expect(user.subscriptions_url).to.equal('https://api.github.com/users/rportugal/subscriptions');
+    expect(user.organizations_url).to.equal('https://api.github.com/users/rportugal/orgs');
+    expect(user.repos_url).to.equal('https://api.github.com/users/rportugal/repos');
+    expect(user.events_url).to.equal('https://api.github.com/users/rportugal/events{/privacy}');
+    expect(user.received_events_url).to.equal('https://api.github.com/users/rportugal/received_events');
+    expect(user.type).to.equal('User');
+    expect(user.site_admin).to.equal(false);
+    expect(user.name).to.equal('Ricardo Portugal');
+    expect(user.company).to.equal('@sky-uk ');
+    expect(user.blog).to.equal(null);
+    expect(user.location).to.equal('London, United Kingdom');
+    expect(user.email).to.equal(null);
+    expect(user.hireable).to.equal(true);
+    expect(user.bio).to.equal(null);
+    expect(user.public_repos).to.equal(24);
+    expect(user.public_gists).to.equal(4);
+    expect(user.num_followers).to.equal(24);
+    expect(user.num_following).to.equal(22);
+    expect(user.created_at).to.equal('2011-05-12T15:14:20Z');
+    expect(user.updated_at).to.equal('2016-08-12T12:49:11Z');
   });
 });
