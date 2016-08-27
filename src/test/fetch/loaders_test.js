f6d85f188832257e220e0ff501ea11a355a4355f
Ricardo Mendes Portugal <ricardofmportugal@gmail.com>
2016-08-27 16:03:29 +0100

    reorganise some tests according to github apis

--- src/test/fetch/loaders_test.js
+++ src/test/fetch/loaders_test.js
@@ -25,23 +25,43 @@ describe('Loaders', () => {
     nock.cleanAll();
   });
 
-  describe('User', () => {
-    it('is able to fetch a user through the loader', async() => {
-      const userName = 'rportugal';
-      const filename = mockUser(userName);
-      const result = await loaders.users.load(userName);
-      const expectedResult = loadExpectedResult(filename);
-      expect(result).to.deep.eq(expectedResult);
+  describe('Activity APIs', () => {
+
+  });
+
+  describe('Users APIs', () => {
+    describe('Single user', () => {
+      it('is able to fetch a user through the loader', async() => {
+        const userName = 'rportugal';
+        const filename = mockUser(userName);
+        const result = await loaders.users.load(userName);
+        const expectedResult = loadExpectedResult(filename);
+        expect(result).to.deep.eq(expectedResult);
+      });
+    });
+
+    describe('Authenticated user', () => {
+      it.skip('is able to fetch the authenticated user through the loader', async() => {
+
+      });
+    });
+
+    describe('All users', () => {
+      it.skip('is able to get all users through the loader', async() => {
+
+      });
     });
   });
 
-  describe('Repository', () => {
-    it('is able to fetch a repo through the loader', async() => {
-      const fullName = 'rportugal/opencv-zbar';
-      const filename = mockRepo(fullName);
-      const result = await loaders.repos.load(fullName);
-      const expectedResult = loadExpectedResult(filename);
-      expect(result).to.deep.eq(expectedResult);
+  describe('Repositories APIs', () => {
+    describe('Repository', () => {
+      it('is able to fetch a repo through the loader', async() => {
+        const fullName = 'rportugal/opencv-zbar';
+        const filename = mockRepo(fullName);
+        const result = await loaders.repos.load(fullName);
+        const expectedResult = loadExpectedResult(filename);
+        expect(result).to.deep.eq(expectedResult);
+      });
     });
   });
 
@@ -55,13 +75,15 @@ describe('Loaders', () => {
     });
   });
 
-  describe('Pull Requests for Repository', () => {
-    it('is able to fetch the pull requests for a repository through the loader', async() => {
-      const fullName = 'graphql/express-graphql';
-      const filename = mockPullRequestsRepo(fullName);
-      const result = await loaders.pullRequestsRepo.load(fullName);
-      const expectedResult = loadExpectedResult(filename);
-      expect(result).to.deep.eq(expectedResult);
+  describe('Pull Requests APIs', () => {
+    describe('Pull Requests for Repository', () => {
+      it('is able to fetch the pull requests for a repository through the loader', async() => {
+        const fullName = 'graphql/express-graphql';
+        const filename = mockPullRequestsRepo(fullName);
+        const result = await loaders.pullRequestsRepo.load(fullName);
+        const expectedResult = loadExpectedResult(filename);
+        expect(result).to.deep.eq(expectedResult);
+      });
     });
   });
 
