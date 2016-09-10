c0d3026fbd1b54a564059d18d65419a6d65bb42a
Ricardo Portugal <ricardofmportugal@gmail.com>
2016-09-10 18:29:11 +0100

    use chai deep equal

--- src/test/schema/user_query_test.js
+++ src/test/schema/user_query_test.js
@@ -2,6 +2,7 @@
 import chai, { expect } from 'chai';
 import dirtyChai from 'dirty-chai';
 import nock from 'nock';
+import fs from 'fs';
 import runQuery from '../helpers/graphql_runner';
 import { mockUser, mockUserNotFound } from '../helpers/mock';
 
@@ -85,42 +86,11 @@ describe('User query', () => {
       mockUser('rportugal');
     });
 
-    it('returns the base fields', async() => {
+    it('returns the base fields', async() =>{
       const result = await runQuery(query);
 
-      expect(nock.isDone()).to.equal(true, "Didn't hit all mock endpoints!");
-      expect(result.errors).to.be.undefined();
-      const user = result.data.user;
-      expect(user.login).to.equal('rportugal');
-      expect(user.id).to.equal(784082);
-      expect(user.avatar_url).to.equal('https://avatars.githubusercontent.com/u/784082?v=3');
-      expect(user.gravatar_id).to.equal('');
-      expect(user.url).to.equal('https://api.github.com/users/rportugal');
-      expect(user.html_url).to.equal('https://github.com/rportugal');
-      expect(user.followers_url).to.equal('https://api.github.com/users/rportugal/followers');
-      expect(user.following_url).to.equal('https://api.github.com/users/rportugal/following{/other_user}');
-      expect(user.gists_url).to.equal('https://api.github.com/users/rportugal/gists{/gist_id}');
-      expect(user.starred_url).to.equal('https://api.github.com/users/rportugal/starred{/owner}{/repo}');
-      expect(user.subscriptions_url).to.equal('https://api.github.com/users/rportugal/subscriptions');
-      expect(user.organizations_url).to.equal('https://api.github.com/users/rportugal/orgs');
-      expect(user.repos_url).to.equal('https://api.github.com/users/rportugal/repos');
-      expect(user.events_url).to.equal('https://api.github.com/users/rportugal/events{/privacy}');
-      expect(user.received_events_url).to.equal('https://api.github.com/users/rportugal/received_events');
-      expect(user.type).to.equal('User');
-      expect(user.site_admin).to.equal(false);
-      expect(user.name).to.equal('Ricardo Portugal');
-      expect(user.company).to.equal('@sky-uk ');
-      expect(user.blog).to.be.null();
-      expect(user.location).to.equal('London, United Kingdom');
-      expect(user.email).to.equal(null);
-      expect(user.hireable).to.equal(true);
-      expect(user.bio).to.equal(null);
-      expect(user.public_repos).to.equal(24);
-      expect(user.public_gists).to.equal(4);
-      expect(user.num_followers).to.equal(24);
-      expect(user.num_following).to.equal(22);
-      expect(user.created_at).to.equal('2011-05-12T15:14:20Z');
-      expect(user.updated_at).to.equal('2016-08-12T12:49:11Z');
+      const expected = JSON.parse(fs.readFileSync('src/test/fixtures/expected/user_query_test.json', 'utf8'));
+      expect(result).to.deep.equal(expected);
     });
 
     // it('returns the list of followers', async() => {
